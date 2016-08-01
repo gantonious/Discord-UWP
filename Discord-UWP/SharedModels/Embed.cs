74b456b87960e8aec56c85ed2ea17832535f064a
George Antonious <antoniou@ualberta.ca>
2016-07-31 19:36:40 -0600

    Added SharedModels

---
+++ Discord-UWP/SharedModels/Embed.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_UWP.SharedModels
+{
+    public struct Embed
+    {
+        [JsonProperty("title")]
+        public string title { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        [JsonProperty("thumbnail")]
+        public EmbedThumbnail Thumbnail { get; set; }
+        [JsonProperty("provider")]
+        public EmbedProvider Provider { get; set; }
+    }
+
+    public struct EmbedThumbnail
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        [JsonProperty("proxy_url")]
+        public string ProxyUrl { get; set; }
+        [JsonProperty("height")]
+        public int Height { get; set; }
+        [JsonProperty("width")]
+        public int Width { get; set; }
+    }
+
+    public struct EmbedProvider
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+}
