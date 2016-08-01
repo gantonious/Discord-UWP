9fb70b687c845546516a39dcdebab622a3266201
George Antonious <antoniou@ualberta.ca>
2016-08-01 13:54:21 -0600

    Added base GatewayEvent

---
+++ Discord-UWP/Gateway/GatewayEvent.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_UWP.Gateway
+{
+    public struct GatewayEvent
+    {
+        [JsonProperty("op")]
+        public string Operation { get; set; }
+        [JsonProperty("d")]
+        public JObject Data { get; set; }
+        [JsonProperty("s")]
+        public int? SequenceNumber { get; set; }
+        [JsonProperty("t")]
+        public string Type { get; set; }
+    }
+}
