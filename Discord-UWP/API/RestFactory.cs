1c8c79b132c9b1b64d59d4dd8232f24cc25be0d1
George Antonious <antoniou@ualberta.ca>
2016-07-31 21:15:16 -0600

    Created RestFactory to access api implementations

---
+++ Discord-UWP/API/RestFactory.cs
@@ -0,0 +1,48 @@
+﻿using Discord_UWP.API.User;
+using Discord_UWP.API.Channel;
+using Discord_UWP.Authentication;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_UWP.API
+{
+    public class RestFactory
+    {
+        private readonly IAuthenticator _authenticator;
+        private readonly DiscordApiConfiguration _apiConfig;
+        
+        public RestFactory(DiscordApiConfiguration config, IAuthenticator authenticator)
+        {
+            _apiConfig = config;
+            _authenticator = authenticator;
+        }
+
+        public IUserApi GetUserApi()
+        {
+            return RestService.For<IUserApi>(GetAuthenticatingHttpClient());
+        }
+
+        public IChannelApi GetChannelApi()
+        {
+            return RestService.For<IChannelApi>(GetAuthenticatingHttpClient());
+        }
+
+        private HttpClient GetAuthenticatingHttpClient()
+        {
+            return new HttpClient(GetAuthenticationHandler())
+            {
+                BaseAddress = new Uri(_apiConfig.BaseUrl)
+            };
+        }
+
+        private HttpClientHandler GetAuthenticationHandler()
+        {
+            return new AuthenticatingHttpClientHandler(_authenticator);
+        }
+    }
+}
